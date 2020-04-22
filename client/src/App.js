631f5497ae6146ccdf649f4abd02d9bbe6298601
Sager <sager.brown@gmail.com>
2020-04-22 17:53:08 -0500

    'yoyo'

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+import Details from './Details';
+import About from './About';
+import { createBrowserHistory } from 'history';
+import CartContainer from './components/CartContainer';
 import './App.css';
+import { Router, Route, Switch } from "react-router-dom";
+import Default from "./components/Default";
+import Checkout from "./components/Checkout";
+import store from './store';
+import ProductsProvider from './Providers/example.provider';
+import { Provider } from 'react-redux';
+import Modal from './Modal';
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      fuck
-    </div>
+    <Provider store={store}>
+      <Header/>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/" component={Body} />
+            <Route exact path="/cart" history={history} component={CartContainer} />
+            <Route exact path='/details/:id' component={Details} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/checkout" history={history} component={Checkout}/>
+            <Route component={Default} />
+          </Switch>
+          <Modal/>
+          <Footer/>
+
+        </Router>
+    </Provider>
+
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
+import ProductsProvider from './Providers/example.provider'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+ReactDOM.render(
+  <ProductsProvider>
+      <App />
+  </ProductsProvider>,
+  document.getElementById("root")
+);
+
+{/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
